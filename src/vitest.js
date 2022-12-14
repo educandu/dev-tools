c5e7c3492a6d0df7a74f16957a2389002fa63b1e
Andreas Helmberger <public@ahelmberger.de>
2022-12-14 22:23:09 +0100

    NOTICKET: Make vitest resolution work with multiple versions

--- src/vitest.js
+++ src/vitest.js
@@ -1,15 +1,32 @@
+import url from 'node:url';
 import path from 'node:path';
 import { execa } from 'execa';
 import { createRequire } from 'node:module';
 
-const require = createRequire(import.meta.url);
-const resolvedVitestPackage = require.resolve('vitest/package.json');
-const index = resolvedVitestPackage.replaceAll('\\', '/').lastIndexOf('/node_modules/vitest/');
-const rootDir = resolvedVitestPackage.substring(0, index);
-const vitestCliModule = path.resolve(rootDir, './node_modules/vitest/vitest.mjs');
+const getVitestBinFromRootPackage = () => {
+  const thisModuleUrl = import.meta.url;
+  const rootPackageIndex = thisModuleUrl.indexOf('/node_modules/');
+  if (rootPackageIndex === -1) {
+    return null;
+  }
+
+  const rootPackageFolderUrl = thisModuleUrl.substring(0, rootPackageIndex);
+  const vitestBinUrl = `${rootPackageFolderUrl}/node_modules/.bin/vitest`;
+  return url.fileURLToPath(vitestBinUrl);
+};
+
+const getVitestCliModuleFromResolve = () => {
+  const require = createRequire(import.meta.url);
+  const resolvedVitestPackage = require.resolve('vitest/package.json');
+  const index = resolvedVitestPackage.replaceAll('\\', '/').lastIndexOf('/node_modules/vitest/');
+  const rootDir = resolvedVitestPackage.substring(0, index);
+  return path.resolve(rootDir, './node_modules/vitest/vitest.mjs');
+};
+
+const vitestToExecute = getVitestBinFromRootPackage() || getVitestCliModuleFromResolve();
 
 function runVitest(command, ...args) {
-  return execa(process.execPath, [vitestCliModule, command, ...args], { stdio: 'inherit' });
+  return execa(process.execPath, [vitestToExecute, command, ...args], { stdio: 'inherit' });
 }
 
 export const vitest = {
