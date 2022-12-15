05f1e752a40b63a352dc9129f9c6a57701454c2d
Andreas Helmberger <public@ahelmberger.de>
2022-12-15 10:58:03 +0100

    NOTICKET: Move vitest from to devDependencies only

--- src/vitest.js
+++ src/vitest.js
@@ -1,32 +1,9 @@
-import url from 'node:url';
-import path from 'node:path';
 import { execa } from 'execa';
-import { createRequire } from 'node:module';
 
-const getVitestBinFromRootPackage = () => {
-  const thisModuleUrl = import.meta.url;
-  const rootPackageIndex = thisModuleUrl.indexOf('/node_modules/');
-  if (rootPackageIndex === -1) {
-    return null;
-  }
-
-  const rootPackageFolderUrl = thisModuleUrl.substring(0, rootPackageIndex);
-  const vitestBinUrl = `${rootPackageFolderUrl}/node_modules/.bin/vitest`;
-  return url.fileURLToPath(vitestBinUrl);
-};
-
-const getVitestCliModuleFromResolve = () => {
-  const require = createRequire(import.meta.url);
-  const resolvedVitestPackage = require.resolve('vitest/package.json');
-  const index = resolvedVitestPackage.replaceAll('\\', '/').lastIndexOf('/node_modules/vitest/');
-  const rootDir = resolvedVitestPackage.substring(0, index);
-  return path.resolve(rootDir, './node_modules/vitest/vitest.mjs');
-};
-
-const vitestToExecute = getVitestBinFromRootPackage() || getVitestCliModuleFromResolve();
+const VITEST_BIN_PATH = './node_modules/.bin/vitest';
 
 function runVitest(command, ...args) {
-  return execa(process.execPath, [vitestToExecute, command, ...args], { stdio: 'inherit' });
+  return execa(process.execPath, [VITEST_BIN_PATH, command, ...args], { stdio: 'inherit' });
 }
 
 export const vitest = {
