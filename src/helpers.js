4025d4c1799977700eae5b24fe1285e0a6811c9b
Andreas Helmberger <public@ahelmberger.de>
2023-01-30 12:29:45 +0100

    EDU-1086 Simplify translation handling by only merging all files together

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,10 @@
+import yaml from 'yaml';
 import os from 'node:os';
 import JSZip from 'jszip';
 import fse from 'fs-extra';
 import path from 'node:path';
 import globModule from 'glob';
+import extend from 'just-extend';
 import { promisify } from 'node:util';
 
 export function delay(ms) {
@@ -17,13 +19,6 @@ export function isMac() {
   return os.platform() === 'darwin';
 }
 
-export function kebabToCamel(str) {
-  return str
-    .replace(/^-+/, '')
-    .replace(/-[a-z0-9]/g, c => c.toUpperCase())
-    .replace(/-/g, '');
-}
-
 export function noop() {}
 
 export async function writeZipFile(fileName, fileMap) {
@@ -35,4 +30,16 @@ export async function writeZipFile(fileName, fileMap) {
 
   await fse.ensureDir(path.dirname(fileName));
   await fse.writeFile(fileName, await archive.generateAsync({ type: 'nodebuffer' }));
+}
+
+export async function mergeYamlFilesToJson({ inputFilesPattern, outputFile }) {
+  const filePaths = await glob(inputFilesPattern);
+  const translations = await Promise.all(filePaths.map(async filePath => {
+    const fileContent = await fse.readFile(filePath, 'utf8');
+    return yaml.parse(fileContent);
+  }));
+
+  const mergedTranslations = extend({}, ...translations);
+
+  await fse.writeJson(outputFile, mergedTranslations, { spaces: 2 });
 }
--- src/helpers.spec.js
+++ src/helpers.spec.js
@@ -1,19 +1,11 @@
-import { kebabToCamel } from './helpers.js';
+import os from 'node:os';
+import { isMac } from './helpers.js';
 import { describe, expect, it } from 'vitest';
 
 describe('helpers', () => {
-  describe('kebabToCamel', () => {
-    const testCases = [
-      { input: 'hello-world', expected: 'helloWorld' },
-      { input: 'hello--world', expected: 'helloWorld' },
-      { input: 'hello--world--', expected: 'helloWorld' },
-      { input: '--hello--world', expected: 'helloWorld' }
-    ];
-
-    testCases.forEach(({ input, expected }) => {
-      it(`should convert '${input}' to '${expected}'`, () => {
-        expect(kebabToCamel(input)).toBe(expected);
-      });
+  describe('isMac', () => {
+    it('should return if the current platform is of type `darwin`', () => {
+      expect(isMac()).toBe(os.platform() === 'darwin');
     });
   });
 });
